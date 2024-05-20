1b8f5dd670abbb835deb8f47d5c7f3ed93491944
Samir Lohiya <129154519+samirlohiya101@users.noreply.github.com>
2024-05-20 21:50:51 +0530

    added http port on node client

--- nodeClient/src/index.ts
+++ nodeClient/src/index.ts
@@ -17,8 +17,26 @@
 import dotenv from 'dotenv';
 dotenv.config()
 import io from 'socket.io-client';
+import http from 'http'; 
+const PORT = process.env.PORT 
 import { performanceData } from './utils/getPerformanceData';
 import { fetchMacAddress } from './utils/fetchMacAdd';
+
+
+
+
+// Adding a http port to deploy on the free cloud provider 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Dummy server running\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`HTTP server listening on port ${PORT}`);
+});
+
+
+
 
 const socket = io(process.env.SOCKET_SERVER_URL  || '');
 
